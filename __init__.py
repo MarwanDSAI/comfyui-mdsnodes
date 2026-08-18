15d41cd2c6824572167d28abc9f0a001318d1d75
Marwan <marwan_support@live.com>
2026-08-17 21:54:20 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -15,12 +15,14 @@ logging.info(f"### Loading: ComfyUI-MDSNodes ({version_str})")
 from .nodes.UNetNameToCKPTName import MarUNetNameToCKPTName
 from .nodes.ControlNetSelect import MarControlNetSelect
 from .nodes.RatioCombobox import MarRatioCombobox
+from .nodes.LoadImageWithPath import MarLoadImageWithPath
 # -------------------------------------------------------
 # Map internal unique string IDs to Python classes
 NODE_CLASS_MAPPINGS = {
     "MarUNetNameToCKPTName": MarUNetNameToCKPTName,
     "MarControlNetSelect": MarControlNetSelect,
     "MarRatioCombobox": MarRatioCombobox,
+     "MarLoadImageWithPath": MarLoadImageWithPath,
 }
 # -------------------------------------------------------
 # Map internal string IDs to the friendly titles shown on the canvas
@@ -28,7 +30,9 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "MarUNetNameToCKPTName": "UNET To CKPT Converter",
     "MarControlNetSelect": "ControlNet Select",
     "MarRatioCombobox": "Ratio Combobox",
+    "MarLoadImageWithPath": "Load Image (With Path)",
 }
 # -------------------------------------------------------
 # Expose the mappings so ComfyUI can register the nodes upon server startup
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
+
