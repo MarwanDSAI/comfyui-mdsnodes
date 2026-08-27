a25c33052d72dadc8bef65375321efb185981163
Marwan <marwan_support@live.com>
2026-08-27 18:29:44 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 # Marwan ComfyUI Custom Nodes
 # -----------------------------------------------------------
 import logging
-version_code = [1, 1, 00]
+version_code = [1, 1, 1]
 version_str = f"V{version_code[0]}.{version_code[1]}" + (f'.{version_code[2]}' if len(version_code) > 2 else '')
 logging.info(f"Loading: ComfyUI-MDSNodes ({version_str})")
 # -----------------------------------------------------------
